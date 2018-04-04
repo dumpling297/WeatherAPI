af308a77b78438c8e627fb6ef57837f6e38389fe
dumpling297 <spaudel@sfu.ca>
2018-04-03 22:31:41 -0700

    added Comments
    Comments were added to the file for clarification. (Done for a technical writing course). Thank you.

--- index.js
+++ index.js
@@ -1,60 +1,57 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const $ = require("jquery");
 const app = express()
 const remote = require('remote-json');
 app.use(bodyParser.json())
-//Temperature.
+
 var globalVariableTemp;
-//Scale of the temperature. Could be Celsius or Fahrenheit.
 var globalVariableScale;
 
-//Default page.
+//send a response once 'home' page is first loaded 
 app.get('/', (req, res, next) => {
   res.send('WeatherAPI is up and running');
   next();
 })
 
-//Handling parameters.
+//handles event of loading page 'locations'
 app.get('/locations/*', (req, res, next) => {
+ 
+  //grab the request message, which will include the form of degree (weather) 
   var zip = 'zip=' + req.params[0];
-  //Gets the parameter scale.
   scale = req.query.scale;
   var units;
+
   if (scale == 'Celsius')
   {
-    //Sets corresponding scale for requesting.
-    units = '&units=metric';
-    //Sets display scale for responding.
-    globalVariableScale = 'Celsius';
+     units = '&units=metric';
+     globalVariableScale = 'Celsius';
   }
   else {
-    //Sets corresponding scale for requesting.
-    units = '&units=imperial'
-    //Sets display scale for responding.
-    globalVariableScale = 'Fahrenheit';
+     units = '&units=imperial'
+     globalVariableScale = 'Fahrenheit';
   }
   var api = 'http://api.openweathermap.org/data/2.5/weather?';
   var apiKey = '&APPID=33617275531e7a92cdd343cf8beccb6a';
   var request = api + zip + units + apiKey;
-  //Gets remote JSON file.
+
   remote(request)
   .get(function (err, res, body) {
+    //grab the temperature value
     var mydata = JSON.parse(body);
     globalVariableTemp = (mydata.main.temp);
   });
-  //Hard-coded asynchronous operation.
   setTimeout(function()
   {
-    //Respond 200 status code.
+    //respond with the two values in loop of the given interval 
     res.status(200);
-    //Respond weather JSON file.
     res.send(JSON.stringify({ temperature: globalVariableTemp, scale: globalVariableScale }));
   }, 600);
 })
 
-//Application port.
+
+//setting the port of the application
 app.set('port', 8080)
-//Show port information on console.
 const server = app.listen(app.get('port'), () => {
   console.log(`Http server listener : PORT ${server.address().port}`)
 })
